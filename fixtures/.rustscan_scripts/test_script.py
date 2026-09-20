9bd4079911e0d5261db0570641c597bc004ba5eb
rezamqds <85675968+rezamqds@users.noreply.github.com>
2026-09-20 17:20:26 +0330

    Fix comment for clarity in test_script.py

--- fixtures/.rustscan_scripts/test_script.py
+++ fixtures/.rustscan_scripts/test_script.py
@@ -4,7 +4,7 @@
 #trigger_port = "80"
 #call_format = "python3 {{script}} {{ip}} {{port}}"
 
-# Sriptfile parser stops at the first blank line with parsing.
+# The Scriptfile parser stops parsing at the first blank line.
 # This script will run itself as an argument with the system installed python interpreter, only scanning port 80.
 # Unused filed: ports_separator = ","
 
